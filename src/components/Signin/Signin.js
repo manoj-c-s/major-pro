f6f77d324ffd6c3b66ac9b954245dcc6c9af8790
manoj-c-s <manojcs123456@gmail.com>
2023-05-25 19:12:49 +0530

    added

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,53 +1,60 @@
-import { Button } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './login.css'
+import { Link, useHistory } from "react-router-dom";
 import { auth } from "../../lib/firebase";
-import Signup from "../Signup/Signup";
-import "./styles.css";
-import { googleProvider } from "../../lib/firebase";
 
-const Signin = () => {
-  const [showSignup, setShowSignup] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const GoogleSignIn = (e) => {
-    e.preventDefault();
-    console.log(googleProvider);
-    auth.signInWithPopup(googleProvider).catch((e) => {
-      alert(e.message);
-    });
-  };
+function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
+    return (
+        <div className='login'>
+      
+
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
+                </form>
 
 
-
-  return (
-    <div className="login">
-      {showSignup ? (
-        <Signup showSignup={setShowSignup} />
-      ) : (
-        <div className="login__content">
-          {loading && <div className="login__loading" />}
-
-          <div className={`login__wrapper ${loading && "login__fade"}`}>
-            <img
-              className="login__logo"
-              src="/assets/svg/google.svg"
-              alt="Google"
-            />
-
-            <p className="login__title">Sign in</p>
-            <p className="login__subtitle">Continue to Gmail</p>
-                <Button
-                  className="login__button"
-                  color="primary"
-                  variant="contained"
-                  onClick={GoogleSignIn}
-                >
-                  Sign in
-                </Button>
-          </div>
+                <button onClick={register} className='login__registerButton'>Create Account</button>
+            </div>
         </div>
-      )}
-    </div>
-  );
-};
+    )
+}
 
-export default Signin;
+export default Login
