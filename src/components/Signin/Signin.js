641b9c0bc5de603815d9e0aae0d30afd341e4cc8
Manoj C S <91310299+manoj-c-s@users.noreply.github.com>
2023-06-02 21:13:37 +0530

    Add files via upload

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,60 +1,74 @@
-import React, { useState } from 'react';
-import './login.css'
+import React, { useState } from "react";
+import "./login.css";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "../../lib/firebase";
 
 function Login() {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const signIn = e => {
-        e.preventDefault();
+  const signIn = (e) => {
+    e.preventDefault();
 
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(auth => {
-                history.push('/')
-            })
-            .catch(error => alert(error.message))
-    }
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const register = e => {
-        e.preventDefault();
+  const register = (e) => {
+    e.preventDefault();
 
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // it successfully created a new user with email and password
-                if (auth) {
-                    history.push('/')
-                }
-            })
-            .catch(error => alert(error.message))
-    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <div className='login'>
-      
+  return (
+    <div className='login'>
+      <div className='login__container'>
+        <h1>Sign-in</h1>
 
-            <div className='login__container'>
-                <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='email'
+            id='email'
+          />
 
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+          <h5>Password</h5>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='password'
+            id='password'
+          />
 
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-
-                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
-                </form>
-
-
-                <button onClick={register} className='login__registerButton'>Create Account</button>
-            </div>
-        </div>
-    )
+          <button
+            type='submit'
+            onClick={signIn}
+            className='login__signInButton'
+            id='login'
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
